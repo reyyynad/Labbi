404a66c4622d1bb072c09f0d8464389222d67b20
reyyynad <s202276760@kfupm.edu.sa>
2025-11-14 23:37:25 +0300

    Service Provider Pages tailwind bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Service_Provider from "./pages/service_provider/Service_Provider";
-import './index.css';
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
